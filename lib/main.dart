9bc169f0f38a7f2045a3235d3d554fc91baa3ca0
Ashlin Darius Govindasamy <adg@adgstudios.co.za>
2025-01-18 23:34:15 +0530

    Patch Routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:trial/appointment/personalised_advice.dart';
-import 'package:trial/hiddenprofilepage/hidden_profile_page';
+ 
 //import 'package:trial/hiddenprofilepage/hidden_profile_page.dart';
 import 'package:trial/infoandresources/information_and_resources.dart';
 import 'package:trial/menu/menu_page.dart';
 import 'package:trial/nearbyresources/nearby_resources.dart';
 import 'package:trial/pages/login_page.dart';  // Your login page
 import 'package:trial/placement/data_storage_page.dart';
-import 'package:trial/placement/help_page';
+import 'package:trial/placement/help_page.dart';
+import 'package:trial/placement/hidden_profile_page.dart';
+ 
 import 'package:trial/placement/logout_page.dart';
 import 'package:trial/placement/notifications_page.dart';
-import 'package:trial/placement/profile_page';
+import 'package:trial/placement/profile_page.dart';
+ 
 import 'package:trial/themes/theme_provider.dart';  // Ensure you have this file for ThemeProvider
 
 void main() {
